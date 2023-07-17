35bf7654ae226998d48f2007186a4eb3dbbf9c3d
AndrewRoy3535 <eda.twinkle@gmail.com>
2023-07-17 17:32:57 +0600

    api fixing

--- utils/utility.ts
+++ utils/utility.ts
@@ -2,6 +2,7 @@ import { signIn } from "next-auth/react";
 
 export const apiUri =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+// export const apiUri = "http://localhost:3000";
 
 export function tConvert(time: any) {
   // Check correct time format and split into components
