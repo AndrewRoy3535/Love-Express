5662ce0b114fa8cde6a73306beb6696658e4e060
Shuvo Anirban Roy <codebuilderit@gmail.com>
2023-07-31 18:20:38 +0600

    remove serverSide

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,40 @@
 "use client";
 import React, { Fragment } from "react";
 import { Box } from "@mui/material";
-import SearchBus from "../component/search_bus/SearchBus";
+// import SearchBus from "../component/search_bus/SearchBus";
 import NavTabs from "../component/tabs/NavTab";
-import axios from "axios";
+// import axios from "axios";
 import { useSession } from "next-auth/react";
 import Login from "./login";
 import Loading from "../component/loading/Loading";
-import { apiUri, axiosOption } from "../utils/utility";
+// import { apiUri, axiosOption } from "../utils/utility";
 
-type Props = {
-  destination: Array<{ place: string; _id: string }>;
-};
+// type Props = {
+//   destination: Array<{ place: string; _id: string }>;
+// };
 
-export async function getServerSideProps({ res }: any) {
-  try {
-    const response = await axios.get(`${apiUri}/api/destinations`, axiosOption);
-    const data = response.data;
-    const destination = data || null;
-    res.setHeader(
-      "Cache-Control",
-      "public, s-maxage=1800, stale-while-revalidate=86400"
-    );
+// export async function getServerSideProps({ res }: any) {
+//   try {
+//     const response = await axios.get(`${apiUri}/api/destinations`, axiosOption);
+//     const data = response.data;
+//     const destination = data || null;
+//     res.setHeader(
+//       "Cache-Control",
+//       "public, s-maxage=1800, stale-while-revalidate=86400"
+//     );
 
-    return {
-      props: { destination },
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      props: { destination: null },
-    };
-  }
-}
-
-export default function Home({ destination }: Props) {
+//     return {
+//       props: { destination },
+//     };
+//   } catch (error) {
+//     console.log(error);
+//     return {
+//       props: { destination: null },
+//     };
+//   }
+// }
+// { destination }: Props
+export default function Home() {
   const { status } = useSession();
 
   if (status === "loading") {
@@ -47,7 +47,7 @@ export default function Home({ destination }: Props) {
         <Login />
       ) : (
         <Fragment>
-          <SearchBus destinations={destination} />
+          {/* <SearchBus destinations={destination} /> */}
           <NavTabs />
         </Fragment>
       )}
